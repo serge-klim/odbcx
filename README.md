1437d73e16e6a7a80a1fcf9a84917b1dbb836560
serge <serge.klim@outlook.com>
2018-12-08 19:11:48 +0000

    mssql db init script has been added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# odbcx
+### odbcx
+
+[![Build status](https://ci.appveyor.com/api/projects/status/iy4ku348wo6ysbam?svg=true)](https://ci.appveyor.com/project/serge-klim/odbcx)
+
 odbc helpers
---
+++ test/sql/mssql.sql
@@ -0,0 +1,21 @@
+﻿USE [master]
+
+CREATE DATABASE test 
+GO
+
+USE [test]
+GO
+
+CREATE TABLE test(
+	id INT IDENTITY(1,1),
+	ts [datetime2](7),
+	TARGET [varchar](100),
+	MESSAGETYPE [varchar](100),
+	N INT,
+	PB [image]
+) 
+GO
+
+insert into test (ts, target, messagetype, n, pb) VALUES (GETDATE(), 'test', 'message 1', 0, 0x0123456789ABCDEF)
+insert into test (ts, target, messagetype, n, pb) VALUES (GETDATE(), 'test', 'message 2', 0, 0xFEDCBA9876543210)
+
